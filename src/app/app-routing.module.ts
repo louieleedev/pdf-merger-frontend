e857e001dd6de5bef3d17d6a9779e20ed3f748c1
Lee, Keonhyeong <Keonhyeong.Lee@adesso.de>
2026-02-09 17:31:47 +0100

    feat: PDF datei wählen und reihenfolge definieren beim Mergen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PdfMergeComponent} from "./pages/pdf-merge/pdf-merge.component";
+import {PdfSplitComponent} from "./pages/pdf-split/pdf-split.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'merge', pathMatch: 'full' },
+  { path: 'merge', component: PdfMergeComponent },
+  { path: 'split', component: PdfSplitComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { PdfMergeComponent } from './pages/pdf-merge/pdf-merge.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import {DragDropModule} from "@angular/cdk/drag-drop";
+import {MatIconModule} from "@angular/material/icon";
+import { HeaderComponent } from './layout/header/header.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { PdfSplitComponent } from './pages/pdf-split/pdf-split.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PdfMergeComponent,
+    HeaderComponent,
+    PdfSplitComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    DragDropModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
